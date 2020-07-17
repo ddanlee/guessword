3a249f0573ecd9d50e2d304931430fcb667bbca3
dannylee <dannylee9295@gmail.com>
2020-07-17 17:04:09 +0900

    Updates

---
+++ precache-manifest.7cf1812939fd8a3ac7be8c3dd103ca51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e016607d0cfcb417ec451aca79bf90cb",
+    "url": "/guessword/index.html"
+  },
+  {
+    "revision": "960c06fcdbb0ad0a054a",
+    "url": "/guessword/static/css/main.94b61dfa.chunk.css"
+  },
+  {
+    "revision": "fb5a15f90f46d555738e",
+    "url": "/guessword/static/js/2.98994b87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guessword/static/js/2.98994b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960c06fcdbb0ad0a054a",
+    "url": "/guessword/static/js/main.649a929f.chunk.js"
+  },
+  {
+    "revision": "27936f824acadbb2221f",
+    "url": "/guessword/static/js/runtime-main.f73f9537.js"
+  }
+]);
